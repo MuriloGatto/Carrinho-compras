bc00c89bfc2bc8c9d6df885c28219192fccfe1ba
murilogatto <murilo.barbosa13@gmail.com>
2025-12-05 07:56:19 -0300

    organizaçao do codigo

--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
-
 function adicionar() {
     //recuperar valores: nome, quantidade e valor
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorProdutoUnitario = produto.split('R$')[1];
     let quantidadeProduto = document.getElementById('quantidade');
-
-
     alert(nomeProduto);
     alert(valorProdutoUnitario);
     alert(quantidadeProduto.value);
-
     let preco = quantidadeProduto.value * valorProdutoUnitario
     alert(preco)
     //calcular o preço sub total
-
     //adicionar esse produto no carrinho
     //atualizar o valor total
 }
